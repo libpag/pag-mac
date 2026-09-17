49d0578f24eccfe18a8d07eef91dc397be9e9577
libpag <libpag@tencent.com>
2026-09-17 17:46:50 +0800

    Push From Landun: update SDK 4.5.98

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// 模板文件：由 submit_cocoapods.sh 根据实际版本号生成 Package.swift，请勿直接修改生成的 Package.swift
 
 import PackageDescription
 
@@ -14,6 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libpag",
-            path: "framework/libpag.xcframework")
+            url: "https://github.com/libpag/pag-mac/releases/download/4.5.98/libpag_4.5.98_macOS_arm64_x86_64.zip",
+            checksum: "99abb686aa91d2317a50386286acca4ff0faf9685ed72693cb14a8061bf98289")
     ]
 )
